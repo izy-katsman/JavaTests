9a68022067d57326da21e266c0b960d81e854dda
Moriarty <42452687+izy-katsman@users.noreply.github.com>
2020-03-30 20:25:09 +0400

    all project

--- src/test/java/StepDefinition/Steps.java
+++ src/test/java/StepDefinition/Steps.java
@@ -5,15 +5,18 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import pages.CommonMethod;
 import pages.MainVkPage;
 import pages.VkLogin;
+
+import java.util.Map;
 
 public class Steps {
 
     WebDriver driver;
     VkLogin objLogin;
 
-    @Given("^Open the Firefox and launch the application$")
+    @Given("^launch firefox  and navigate to the vk$")
     public void openTheFirefoxAndNavigateToTheVk() throws Throwable
     {
         System.setProperty("webdriver.gecko.driver", "E:\\Tests\\POM\\geckodriver.exe");
@@ -22,19 +25,20 @@ public class Steps {
         objLogin = new VkLogin(driver);
     }
 
-    @When("^ Login with login'' and password$")
+    @When("^I try to login \"([^\"]*)\" with Password \"([^\"]*)\"$")
     public void LoginInVk(String login, String password) throws Throwable
     {
         objLogin.loginToVk(login, password);
         System.out.println("This step enter the Username and Password on the login page.");
     }
 
-    @Then("^Reset the credential$")
+    @Then("^I should see that I logged in$")
     public void ResetTheStatus() throws Throwable
     {
+        CommonMethod await = new CommonMethod(driver);
         MainVkPage objHomePage = new MainVkPage(driver);
 
-        System.out.println("This step click on the Reset button." + objHomePage.getNameFromMainPage().toLowerCase().contains("izy"));
+        System.out.println("You login - " + objHomePage.getNameFromMainPage().toLowerCase().contains("izy"));
     }
 
 }
